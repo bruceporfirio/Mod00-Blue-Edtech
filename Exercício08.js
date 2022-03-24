e9157dc48226da92ecf7ab4206215925c415d99b
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-24 20:58:55 -0300

    Exercício Resolvido.

--- Exercício08.js
+++ Exercício08.js
@@ -7,4 +7,17 @@
 // PESO CHILENO
 // Para esse exercício você precisará realizar uma pesquisa para saber a cotação de cada moeda em real. 
 // Mostrar o resultado no formato U$9999.99
+const prompt = require('prompt-sync')(); 
+
+let dolar = 4.83
+let euro = 5.31
+let libraEsterlina = 6.37
+let dolarCanadense = 3.85
+let pesoArgentino = 0.044
+let pesoChileno = 0.006
+
+let real = +prompt('Digite o valor em real que deseja converter: ');
+
+console.log('Valores convertidos em real:')
+console.log(`Dólar: R${(dolar * real).toFixed(2)} \nEuro: R$${(euro * real).toFixed(2)} \nLibra Esterlina: R$${(libraEsterlina * real).toFixed(2)} \nDolar Canadense: R$${(dolarCanadense * real).toFixed(2)} \nPeso Argentino: R$${(pesoArgentino * real).toFixed(2)} \nPeso Chileno: R$${(pesoChileno* real).toFixed(2)} `);
 
