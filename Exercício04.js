c1c885384c7b7e6a9231949d229cbcd5a6f3ba69
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-24 19:07:52 -0300

    Exercício resolvido.

--- Exercício04.js
+++ Exercício04.js
@@ -1,2 +1,28 @@
 // Faça um programa de cadastro de clientes que mostre um menu de opções e permita 
 // com que a pessoa escolha umas das opções, exibindo qual foi a opção escolhida.
+const prompt = require('prompt-sync')(); //Adicionar Prompt
+
+let opcao1 = 'Hambúrguer'
+let opcao2 = 'Batata Frita'
+let opcao3 = 'Hambúrguer Vegetariano'
+
+console.log('Menu do Restaurante')
+console.log('1. Batata Frita')
+console.log('2. Hambúrguer')
+console.log('3. Hambúrguer Vegetariano')
+
+
+
+let escolhaCliente = parseInt(prompt('Digite a letra correspondente ao seu pedido:'));
+
+if(escolhaCliente == 1) {
+    console.log(`Você escolheu: ${opcao1}`);
+} else if (escolhaCliente == 2) {
+    console.log(`Você escolheu: ${opcao2}`)
+} else if (escolhaCliente == 3) {
+    console.log(`Você escolheu: ${opcao3}`)
+} else {
+    console.log('Escolha uma ENTRADA válida!')
+}
+
+
