0028516a4e81a4d8478282615564935406f38d1b
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-24 18:02:03 -0300

    Exercício resolvido.

--- Exercício02.js
+++ Exercício02.js
@@ -1,1 +1,5 @@
-// Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!"
+// Elabore um programa que escreve seu nome completo na primeira linha, 
+// seu endereço na segunda e o CEP e telefone na terceira.
+console.clear();
+console.log('Nome completo: Bruce Porfirio \nEndereço: Passagem do Maciambu n86 \nCep: 88048315 Tel.: 48991879252');
+// \n é utilizado para "quebra de linha".
