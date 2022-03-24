db9cb5dab056a71c55fc81d42d9483ea573bad38
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-24 18:33:14 -0300

    Exercício Resolvido e instalação de Prompt

--- Exercício03.js
+++ Exercício03.js
@@ -1,1 +1,10 @@
 // Faça um programa que mostre na tela uma letra de música que você gosta.
+console.clear(); //limpar terminal
+const prompt = require('prompt-sync')(); //Adicionar Prompt
+console.log('O anjo mais velho - Música de O Teatro Mágico'); //mostrar nome da música
+prompt('Pressione ENTER para revelar a letra da música!');//ENter para continuar
+
+console.log(); // pular linha
+
+console.log('Enquanto houver você do outro lado\nAqui do outro eu consigo me orientar\nA cena repete, a cena se inverte\nEnchendo a minhalma daquilo\nQue outrora eu deixei de acreditar\nTua palavra, tua história\nTua verdade fazendo escola\nE tua ausência fazendo silêncio em todo lugar\nMetade de mim agora é assim\nDe um lado a poesia, o verbo, a saudade\nDo outro a luta, a força e a coragem pra chegar no fim\nE o fim é belo incerto, depende de como você vê\nO novo, o credo, a fé que você deposita em você e só\nSó enquanto eu respirar\nVou me lembrar de você\nSó enquanto eu respirar\nSó enquanto eu respirar\nEnquanto houver você do outro lado\nAqui do outro eu consigo me orientar\nA cena repete, a cena se inverte\nEnchendo a minhalma daquilo\nQue outrora eu deixei de acreditar\nTua palavra, tua história\nTua verdade fazendo escola\nE tua ausência fazendo silêncio em todo lugar\nMetade de mim agora é assim\nDe um lado a poesia, o verbo, a saudade\nDo outro a luta, a força e a coragem pra chegar no fim\nE o fim é belo incerto, depende de como você vê\nO novo, o credo, a fé que você deposita em você e só\nSó enquanto eu respirar\nVou me lembrar de você\nSó enquanto eu respirar\nSó enquanto eu respirar\nVou me lembrar de você\nSó enquanto eu respirar\no fim é belo incerto, depende de como você vê\nO novo, o credo, a fé que você deposita em você e só\nSó enquanto eu respirar\nVou me lembrar de você\nSó enquanto eu respirar');
+
