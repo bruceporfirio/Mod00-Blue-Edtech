faa8c31cf8be385939b92f2e816ef0e068fa8d17
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-25 14:04:12 -0300

    Exercícios Resolvidos.

---
+++ 3.Exercício02Difícil.js
@@ -0,0 +1,2 @@
+// Faça um programa que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando 
+// um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições:
