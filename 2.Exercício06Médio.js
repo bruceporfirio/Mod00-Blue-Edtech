728d1f4af3d6ac94c97bf274983d0333f248e09c
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-29 19:04:03 -0300

    Em processo de resolução.

--- 2.Exercício06Médio.js
+++ 2.Exercício06Médio.js
@@ -1,3 +1,13 @@
 // Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. 
 // Caso o número já esteja lá dentro, ele não será adicionado. No final, serão exibidos todos os 
 // valores únicos digitados, em ordem crescente.
+const prompt = require('prompt-sync')();
+
+let listaNumerica = [];
+
+while (listaNumerica >= 10) {
+    let listaNumerica = (+prompt(`Digite um número: `)).push
+}
+// listaNumerica.push = +prompt(`Digite números: `)
+
+// console.log(listaNumerica)
