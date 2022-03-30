03ca3b2c73d68604897bb2a9636813f22a13f5f5
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-03-29 21:30:31 -0300

    Exercícios Resolvidos.

--- 3.Exercício01Difícil.js
+++ 3.Exercício01Difícil.js
@@ -1,2 +1,13 @@
 // Faça um programa, que receba as dimensões de um terreno retangular (largura e comprimento) e
 //  mostre a área do terreno:
+
+const prompt = require('prompt-sync')(); 
+
+console.log(`Vamos calcular a área do seu terreno!`);
+
+let largura = +prompt(`Digite a largura do seu terreno: `);
+let comprimento = +prompt(`Digite o comprimento do seu terreno: `)
+
+let area =  (largura * comprimento).toFixed(2);
+
+console.log(`Seu terreno tem ${area} metros de área!`);
--- 3.Exercício02Difícil.js
+++ 3.Exercício02Difícil.js
@@ -1,2 +1,12 @@
 // Faça um programa que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando 
 // um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições:
+const prompt = require('prompt-sync')(); 
+console.log(`Descubra se você tem idade para votar nessa eleição!`);
+anoNascimento = +prompt(`Em que ano você nasceu: `)
+let resultado = 2022 - anoNascimento;
+
+if (resultado >= 16) {
+    console.log(`Você tem idade para votar nesta eleição!`)
+} else if (resultado < 16) {
+    console.log(`Você não tem idade suficiente para votar nesta eleição.`)
+}
